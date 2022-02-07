e48ee65b563aa21d97bbaf84b4aad76968ee12f9
Szymon Szaminko <sz.szaminko@gmail.com>
2022-02-07 01:01:39 +0100

    Create a method for variable examples with using Scanner

--- fundamentals/src/Fundamentals.java
+++ fundamentals/src/Fundamentals.java
@@ -1,6 +1,42 @@
+import java.util.Scanner;
+
 public class Fundamentals {
 
     public static void main(String[]args){
-        System.out.println("Let's learning Java basic!");
+        //Start information
+        //System.out.println("Let's learning Java basic!");
+
+      //Variables: String, int, boolean
+        giveYourData();
+    }
+
+    public static void giveYourData(){
+
+        Scanner scanner = new Scanner(System.in);
+        //String
+        System.out.println("Give your name: ");
+        String name = scanner.nextLine();
+        //Integer
+        System.out.println("Give your age: ");
+        int age = Integer.parseInt(scanner.nextLine());
+        //Boolean
+        System.out.println("Do you like programming? (yes/no): ");
+        boolean doYouLikeProgramming;
+        String message;
+        String answer = scanner.nextLine();
+        if(answer.equals("yes")){
+            doYouLikeProgramming = true;
+        }else if(answer.equals("no")){
+            doYouLikeProgramming=false;
+        }
+
+        if(doYouLikeProgramming = true){
+            message = "you like programming :)";
+        }else{
+            message = "you don't like programming";
+        }
+
+        System.out.println("Your name is "+name+", you "+age+" years old and "+message);
+
     }
 }
