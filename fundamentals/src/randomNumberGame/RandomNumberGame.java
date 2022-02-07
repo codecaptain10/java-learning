f620392ffb8eff19b559e06ad44987c2ebb5e6d0
Szymon Szaminko <sz.szaminko@gmail.com>
2022-02-07 16:02:52 +0100

    Add a attempt counter

--- fundamentals/src/randomNumberGame/RandomNumberGame.java
+++ fundamentals/src/randomNumberGame/RandomNumberGame.java
@@ -13,30 +13,31 @@ public class RandomNumberGame {
         System.out.println("----- Play game! -----");
 
         //Counter how many times you try
-        int counter;
+        int counter = 0;
         //Random number
         Random random = new Random();
         int randomNumber = random.nextInt(100)+1;
-        checkNumber(randomNumber);
+        checkNumber(randomNumber,counter);
 
 
     }
 
-    public static void checkNumber(int randomNumber){
+    public static void checkNumber(int randomNumber, int counter){
 
         int userNumber;
         System.out.println("Give your number: ");
         Scanner scanner = new Scanner(System.in);
         userNumber = Integer.parseInt(scanner.nextLine());
+        counter++;
 
         if(userNumber > randomNumber){
             System.out.println("Too BIG...Try again!");
-            checkNumber(randomNumber);
+            checkNumber(randomNumber,counter);
         }else if(userNumber < randomNumber){
             System.out.println("Too SMALL...Try again!");
-            checkNumber(randomNumber);
+            checkNumber(randomNumber,counter);
         }else{
-            System.out.println("YEEES! That was "+randomNumber+". You won!");
+            System.out.println("YEEES! That was "+randomNumber+". You won in "+counter+ " attempt.");
             System.out.println("----- FINISHED -----");
         }
 
