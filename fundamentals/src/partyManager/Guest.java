13576fd319e0ffa54da2e0d4a5729b544f569c49
Szymon Szaminko <sz.szaminko@gmail.com>
2022-02-08 15:11:57 +0100

    Create class Guest

---
+++ fundamentals/src/partyManager/Guest.java
@@ -0,0 +1,47 @@
+package partyManager;
+
+public class Guest {
+    private String name;
+    private String meal;
+    private int phoneNumber;
+    private boolean isVegan;
+
+    public Guest(String name, String meal, int phoneNumber, boolean isVegan) {
+        this.name = name;
+        this.meal = meal;
+        this.phoneNumber = phoneNumber;
+        this.isVegan = isVegan;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMeal() {
+        return meal;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public boolean isVegan() {
+        return isVegan;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMeal(String meal) {
+        this.meal = meal;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setVegan(boolean vegan) {
+        isVegan = vegan;
+    }
+}
