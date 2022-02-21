e2b9e13195ad7ea0a8503cda794eb36be1fdae1b
Szymon Szaminko <sz.szaminko@gmail.com>
2022-02-21 23:49:29 +0100

    Create a App class to run computer simulator

---
+++ oop/src/computerSimulator/App.java
@@ -0,0 +1,8 @@
+package computerSimulator;
+
+public class App {
+    public static void main(String[]args){
+        Monitor monitor = new Monitor();
+        System.out.println("Hello Monitor!");
+    }
+}
